ab536623ea8cb9037e72e30d2d9a1d744d19169a
Shaunak Kale <epicshaunak@gmail.com>
2017-08-07 11:47:26 -0700

    s

--- car/car.ino
+++ car/car.ino
@@ -2,11 +2,11 @@
 #include <CurieIMU.h>
 
 #include <Servo.h>
-
+float serialval=0;
 int STBY = 6; //standby
 
 //Motor A
-int PWMA = 9; //Speed control 
+int PWMA = 9; //Speed control
 int AIN1 = 7; //Direction
 int AIN2 = 8; //Direction
 
@@ -20,33 +20,34 @@ void loop();
 void move(int motor, int speed, int direction);
 void stop();
 void setup(){
-  pinMode(13,INPUT);
-  Serial.begin(9600);
-  Serial1.begin(115200);
-  pinMode(STBY, OUTPUT);
-  pinMode(PWMA, OUTPUT);
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
-  pinMode(PWMB, OUTPUT);
-  pinMode(BIN1, OUTPUT);
-  pinMode(BIN2, OUTPUT);
-  pinMode(13, INPUT); 
-  pinMode(A1, INPUT); 
-  pinMode(A2, INPUT); 
-  pinMode(A3, INPUT); 
-  pinMode(A4, INPUT); 
-  pinMode(A5, INPUT);   
+        pinMode(13,INPUT);
+        Serial.begin(9600);
+        Serial1.begin(115200);
+        pinMode(STBY, OUTPUT);
+        pinMode(PWMA, OUTPUT);
+        pinMode(AIN1, OUTPUT);
+        pinMode(AIN2, OUTPUT);
+        pinMode(PWMB, OUTPUT);
+        pinMode(BIN1, OUTPUT);
+        pinMode(BIN2, OUTPUT);
+        pinMode(13, INPUT);
+        pinMode(A1, INPUT);
+        pinMode(A2, INPUT);
+        pinMode(A3, INPUT);
+        pinMode(A4, INPUT);
+        pinMode(A5, INPUT);
 }
 
 void loop(){
-   if (Serial.available()) {      // If anything comes in Serial (USB),
-    Serial1.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
-  }
+        
+        if (Serial.available()) { // If anything comes in Serial (USB),
+                Serial1.write(Serial.read()); // read it and send it out Serial1 (pins 0 & 1)
+        }
 
-  if (Serial1.available()) {     // If anything comes in Serial1 (pins 0 & 1)
-    Serial.write(Serial1.read());   // read it and send it out Serial (USB)
-  }
- 
+        if (Serial1.available()) { // If anything comes in Serial1 (pins 0 & 1)
+                Serial.write(Serial1.read()); // read it and send it out Serial (USB)
+        }
+        
 
 }
 
@@ -57,29 +58,28 @@ void move(int motor, int speed, int direction){
 //speed: 0 is off, and 255 is full speed
 //direction: 0 clockwise, 1 counter-clockwise
 
-  digitalWrite(STBY, HIGH); //disable standby
+        digitalWrite(STBY, HIGH); //disable standby
 
-  boolean inPin1 = LOW;
-  boolean inPin2 = HIGH;
+        boolean inPin1 = LOW;
+        boolean inPin2 = HIGH;
 
-  if(direction == 1){
-    inPin1 = HIGH;
-    inPin2 = LOW;
-  }
+        if(direction == 1) {
+                inPin1 = HIGH;
+                inPin2 = LOW;
+        }
 
-  if(motor == 1){
-    digitalWrite(AIN1, inPin1);
-    digitalWrite(AIN2, inPin2);
-    analogWrite(PWMA, speed);
-  }else{
-    digitalWrite(BIN1, inPin1);
-    digitalWrite(BIN2, inPin2);
-    analogWrite(PWMB, speed);
-  }
+        if(motor == 1) {
+                digitalWrite(AIN1, inPin1);
+                digitalWrite(AIN2, inPin2);
+                analogWrite(PWMA, speed);
+        }else{
+                digitalWrite(BIN1, inPin1);
+                digitalWrite(BIN2, inPin2);
+                analogWrite(PWMB, speed);
+        }
 }
 
 void stop(){
-//enable standby  
-  digitalWrite(STBY, LOW); 
+//enable standby
+        digitalWrite(STBY, LOW);
 }
-

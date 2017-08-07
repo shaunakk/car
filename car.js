bf609f40e9fcc3438b1991ed4958392ffc0afb7f
Shaunak Kale <epicshaunak@gmail.com>
2017-08-06 20:28:02 -0700

    s

--- car.js
+++ car.js
@@ -14,7 +14,7 @@ io.on('connection', function(socket) {
   console.log('A user connected');
 
   socket.on('joystickData', function(data) {
-    console.log(Math.atan(parseFloat(data[0]) / parseFloat(data[1])) * radtodeg);
+    console.log(Math.atan2(parseFloat(data[0]), parseFloat(data[1])) * radtodeg);
 
   });
   socket.on('disconnect', function() {

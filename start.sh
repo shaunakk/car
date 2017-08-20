a7a448c4cf76787334c636ceef6545d54a653c7e
Shaunak Kale <epicshaunak@gmail.com>
2017-08-20 00:39:41 +0000

    hi

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ v4l2-ctl -p 10
 v4l2-ctl -c sharpness=100
 cd /home/pi/car
 npm start &
-cvlc --no-audio v4l2:///dev/video0 --v4l2-width 1280 --v4l2-height 720 --v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 --sout '#standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8554/}' -I dummy &
+sudo nice -n -20 su -c ` cvlc --no-audio v4l2:///dev/video0 --v4l2-width 1280 --v4l2-height 720 --v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 --sout '#standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8554/}' -I dummy` &
